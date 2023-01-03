c09bf85ff6dabe6e452004271911cd4824a90912
Sam <samgkoch@hotmail.com>
2023-01-03 12:01:19 +0100

    add BR Tiles

--- app.py
+++ app.py
@@ -3,41 +3,38 @@ from flask import Flask, render_template
 from bokeh.embed import components
 from bokeh.plotting import figure
 from bokeh.resources import INLINE
+from bokeh.models import WMTSTileSource
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+  return 'Hello, Sam!'
 
 @app.route('/bokeh')
 def bokeh():
+  tile_provider = WMTSTileSource(
+      url='http://tiles.buienradar.nl/tiles-eu-v2/{Z}/{X}/{Y}.png',
+      attribution='Test by Sam with Buienradar Tiles'
+  )
 
-    # init a basic bar chart:
-    # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
-    fig = figure(plot_width=600, plot_height=600)
-    fig.vbar(
-        x=[1, 2, 3, 4],
-        width=0.5,
-        bottom=0,
-        top=[1.7, 2.2, 4.6, 3.9],
-        color='navy'
-    )
+  # range bounds supplied in web mercator coordinates
+  fig = figure(x_range=(230000, 880000), y_range=(6750000, 6900000),
+            x_axis_type="mercator", y_axis_type="mercator")
+  fig.add_tile(tile_provider)
 
-    # grab the static resources
-    js_resources = INLINE.render_js()
-    css_resources = INLINE.render_css()
+  # grab the static resources
+  js_resources = INLINE.render_js()
+  css_resources = INLINE.render_css()
 
-    # render template
-    script, div = components(fig)
-    html = render_template(
-        'index.html',
-        plot_script=script,
-        plot_div=div,
-        js_resources=js_resources,
-        css_resources=css_resources,
-    )
-    return html
+  # render template
+  script, div = components(fig)
+  html = render_template(
+      'index.html',
+      plot_script=script,
+      plot_div=div,
+      js_resources=js_resources,
+      css_resources=css_resources,
+  )
 
-if __name__ == '__main__':
-    app.run(debug=True)
+  return html
